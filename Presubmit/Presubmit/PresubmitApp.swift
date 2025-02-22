a4f63dd7034207b15456f369b5b8abb3d27b9ba7
pranesh <praneshv37@gmail.com>
2025-02-22 00:09:11 -0500

    Initial Folders

---
+++ Presubmit/Presubmit/PresubmitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresubmitApp.swift
+//  Presubmit
+//
+//  Created by Pranesh Velmurugan on 2/22/25.
+//
+
+import SwiftUI
+
+@main
+struct PresubmitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Presubmit/PresubmitTests/PresubmitTests.swift
@@ -0,0 +1,17 @@
+//
+//  PresubmitTests.swift
+//  PresubmitTests
+//
+//  Created by Pranesh Velmurugan on 2/22/25.
+//
+
+import Testing
+@testable import Presubmit
+
+struct PresubmitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
