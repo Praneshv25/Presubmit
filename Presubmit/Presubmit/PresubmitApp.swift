a30ea3ae45c59f53e909fb25d9f1f18100ccfa80
Vincent Zhao <59675522+VincentZhao12@users.noreply.github.com>
2025-02-22 05:29:22 -0500

    firebase
    Signed-off-by: Vincent Zhao <59675522+VincentZhao12@users.noreply.github.com>

--- Presubmit/Presubmit/PresubmitApp.swift
+++ Presubmit/Presubmit/PresubmitApp.swift
@@ -1,14 +1,44 @@
-//
-//  PresubmitApp.swift
-//  Presubmit
-//
-//  Created by Pranesh Velmurugan on 2/22/25.
-//
-
 import SwiftUI
+import FirebaseCore
+import FirebaseAuth
+import Firebase
+import UIKit
+import GoogleSignIn
+
+class AppDelegate: NSObject, UIApplicationDelegate { // Remove GIDToken - it's not needed here
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+
+        GIDSignIn.sharedInstance.restorePreviousSignIn { result, error in
+            if error != nil {
+                // Handle error or simply proceed as a new sign-in flow will happen later
+                print("Error restoring previous sign-in: \(error?.localizedDescription ?? "Unknown error")")
+            } else {
+                // User already signed in previously, you can update your UI accordingly
+                guard let user = result else { return }
+                print("User restored from previous sign-in: \(user.profile?.name ?? "User")")
+                // You might want to notify your app state here that a user is already signed in.
+            }
+        }
+
+        return true
+    }
+
+    // Handle URL callback for Google Sign-in
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
+    }
+}
 
 @main
 struct PresubmitApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
